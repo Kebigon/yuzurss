f57271cdf809b549ec3a691c493e3a62cfb99088
Kebigon <git@kebigon.xyz>
2019-01-02 12:01:18 +0900

    Working on packaging

--- src/main/external-resources/yuzurss.sh
+++ src/main/external-resources/yuzurss.sh
@@ -15,9 +15,9 @@ else
   ERROR="-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=../var/log/$SERVICE_NAME-$(date +%Y%m%d-%H%M%S).hprof -XX:ErrorFile=../var/log/$SERVICE_NAME-$(date +%Y%m%d-%H%M%S)-error.log"
 
   GC="-XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:GCPauseIntervalMillis=500 -XX:+DisableExplicitGC"
-  GC_LOGS="-Xlog:gc*=trace:file=../var/log/$SERVICE_NAME-$(date +%Y%m%d-%H%M%S)-gc.log:tags,time,uptime,level"
+  GC_LOGS="-XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:../var/log/$SERVICE_NAME-$(date +%Y%m%d-%H%M%S)-gc.log"
 
-  CLASSPATH="-cp ../cfg:../lib/*"
+  CLASSPATH="-cp ../cfg:../ext/*:../lib/*"
 
   screen -dmS $SERVICE_NAME java -server $HEAP $ERROR $GC $GC_LOGS $CLASSPATH $SERVICE_MAIN
 
